5dc31b80093f824463271c4fb36748994604706a
Anastasya Nosevich <nastianosevich@gmail.com>
2020-02-09 14:48:14 +0300

    feat: add Quiz and Game component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent/Header';
 import Quiz from './QuizComponent/Quiz';
+import Game from './GameComponent/Game';
+import NextLevel from './NextLevelComponent/NextLevel';
+import './styles/main.scss';
 
 class App extends Component {
   render() {
@@ -8,6 +11,8 @@ class App extends Component {
       <div className="wrapper">
         <Header />
         <Quiz />
+        <Game />
+        <NextLevel />
       </div>
     );
   }
