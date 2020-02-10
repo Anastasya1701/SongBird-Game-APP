ab7741fdd0799331386b42c67e27771959e9bd4f
Anastasya Nosevich <nastianosevich@gmail.com>
2020-02-10 23:24:11 +0300

    feat: add deploy version

---
+++ precache-manifest.739180a6e005d688a8e06500cacfae3d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119313957aafec688fc3a60aaed0ee81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "356bbccd649200c39bd4",
+    "url": "/static/css/2.0036de3e.chunk.css"
+  },
+  {
+    "revision": "30b9a3fe6e1de608c2b9",
+    "url": "/static/css/main.968f27cf.chunk.css"
+  },
+  {
+    "revision": "356bbccd649200c39bd4",
+    "url": "/static/js/2.243b2a68.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.243b2a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b9a3fe6e1de608c2b9",
+    "url": "/static/js/main.09d64429.chunk.js"
+  },
+  {
+    "revision": "8146f0cabe9c8f6aeb56",
+    "url": "/static/js/runtime-main.cad64008.js"
+  },
+  {
+    "revision": "d07ffa04b3f14e723afa8f214d0b89f2",
+    "url": "/static/media/Sertificate-bird.d07ffa04.pdf"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/static/media/bird.06a46938.jpg"
+  },
+  {
+    "revision": "4f82cd73f3446ef1af7fa6d3a302ef3a",
+    "url": "/static/media/logo.4f82cd73.svg"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/static/media/roboto-v20-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "169a3a76c7f8ae0911b6ea9fe098d000",
+    "url": "/static/media/success.169a3a76.wav"
+  },
+  {
+    "revision": "5acd1a089942ad6d903b600a76ef063c",
+    "url": "/static/media/wrong.5acd1a08.mp3"
+  }
+]);
